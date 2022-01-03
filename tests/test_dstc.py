4055cd56c49959de88f8f18e5689bb220ae42b09
Yeonchan <acha21@europa.snu.ac.kr>
2022-01-03 09:25:53 +0900

    updated README.md

--- tests/test_dstc.py
+++ tests/test_dstc.py
@@ -2,11 +2,5 @@ import unittest
 from unittest import TestCase
 import angt.eval.dstc as dstc
 
-
-class DSTCTest(TestCase):
-    def test_foo(self):
-        self.assertEqual(dstc.foo(2,3), 5)
-
-
 if __name__ == "__main__":
     unittest.main()
